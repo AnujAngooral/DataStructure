b6b43e7ca5ed0ca12ce61a5c1bc8fdb42c6dcf95
Ankita Sharma <jhingan.ankita@gmail.com>
2023-11-05 11:06:27 +0100

    [TAS-1] - Set-matrix-zeroes

---
+++ Array/SetMatrixZeroes.cs
@@ -0,0 +1,45 @@
+﻿
+namespace DataStructure.Array
+{
+    internal class SetMatrixZeroes
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+
+            var rowArray = new int[rows];
+            var colArray = new int[cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+
+                for (int j = 0; j < cols; j++)
+                {
+
+                    if (matrix[i][j] == 0)
+                    {
+                        rowArray[i] = 1;
+                        colArray[j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+
+                for (int j = 0; j < cols; j++)
+                {
+
+                    if (rowArray[i] == 1 || colArray[j] == 1)
+                    {
+
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+        }
+    }
+}
